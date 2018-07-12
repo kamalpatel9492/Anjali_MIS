b912a13256390243b5fbf48ecc7efe330686874c
AnjaliMIS <www.patelkamal@gmail.com>
2018-07-13 01:44:38 +0530

    s

---
+++ AnjaliMIS/ViewModals/MenuViewModal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnjaliMIS.ViewModals
+{
+    public class MenuViewModal
+    {
+        public int ModuleID { get; set; }
+        public string ModuleName { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string URL { get; set; }
+        public string IconName { get; set; }
+        public bool StrictlyStop { get; set; }
+        public int Sequence { get; set; }
+        public int UserID { get; set; }
+    }
+}
