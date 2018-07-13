5e9534ffa57791b995b8d7f0d62f40540e0f9182
AnjaliMIS <www.patelkamal@gmail.com>
2018-07-13 13:47:20 +0530

    s

--- AnjaliMIS/Controllers/HomeController.cs
+++ AnjaliMIS/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -58,20 +59,47 @@ namespace AnjaliMIS.Controllers
         {
             DB_A157D8_AnjaliMISEntities1 usersEntities = new DB_A157D8_AnjaliMISEntities1();
             List<MenuViewModal> _MenuViewModal = new List<MenuViewModal>();
-            var data = usersEntities.PR_SEC_UserPrivileges_SelectByUserID(Convert.ToInt32(Session["UserID"].ToString())).ToList();
+            // var data = usersEntities.PR_SEC_UserPrivileges_SelectByUserID(Convert.ToInt32(Session["UserID"].ToString())).ToList();
 
-            _MenuViewModal = data.Select(dataElement => new MenuViewModal()
+
+            using (DB_A157D8_AnjaliMISEntities1 db = new DB_A157D8_AnjaliMISEntities1())
             {
-                ModuleID = dataElement.ModuleID,
-                ModuleName = dataElement.ModuleName,
-                Controller = dataElement.Controller,
-                Action = dataElement.Action,
-                URL = dataElement.URL,
-                IconName = dataElement.IconName,
-                StrictlyStop = dataElement.StrictlyStop,
-                Sequence = dataElement.Sequence,
-                UserID = dataElement.UserID
-            }).ToList();
+                try
+                {
+                    //var outParam = new SqlParameter("@UserID", SqlDbType.Int);
+                    //outParam.Direction = ParameterDirection.Output;
+
+                    var sql = "exec PR_SEC_UserPrivileges_SelectByUserID @UserID";
+
+                    List<SqlParameter> parameterList = new List<SqlParameter>();
+                    parameterList.Add(new SqlParameter("@UserID", Convert.ToInt32(Session["UserID"].ToString())));
+
+                    //parameterList.Add(outParam);
+                    SqlParameter[] parameters = parameterList.ToArray();
+
+                    var data1 = db.Database.SqlQuery<MenuViewModal>(sql, parameters).ToList();
+
+                    _MenuViewModal = data1.Select(dataElement => new MenuViewModal()
+                    {
+                        ModuleID = dataElement.ModuleID,
+                        ModuleName = dataElement.ModuleName,
+                        Controller = dataElement.Controller,
+                        Action = dataElement.Action,
+                        URL = dataElement.URL,
+                        IconName = dataElement.IconName,
+                        StrictlyStop = dataElement.StrictlyStop,
+                        Sequence = dataElement.Sequence,
+                        UserID = dataElement.UserID
+                    }).ToList();
+                }
+                catch (Exception ex)
+                {
+                    //SaveErrorLog("JobRepository", "GetJobListByPaging", ex);
+
+                }
+            }
+
+
             return PartialView("_MainMenu", _MenuViewModal);
             //return PartialView(_MenuViewModal);
         }
