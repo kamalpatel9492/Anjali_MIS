c4ad9f535dc18946077bd4cfab4447a65dfe149f
hemang13 <padiahemang@gmail.com>
2018-08-27 02:27:11 +0530

    item issue return changed.

--- AnjaliMIS/Controllers/INV_ItemIssueReturnController.cs
+++ AnjaliMIS/Controllers/INV_ItemIssueReturnController.cs
@@ -23,6 +23,33 @@ namespace AnjaliMIS.Controllers
             return View();
         }
 
+        public ActionResult Issue()
+        {
+            List<INV_StockHistory> model = new List<INV_StockHistory>();
+            model = db.INV_StockHistory.Where(e => e.Remarks == "Issue").ToList();
+           
+            return View(model);
+        }
+
+        public ActionResult AddIssueItem()
+        {
+            var model = new INV_InvoiceViewModal();
+            return View();
+        }
+
+        public ActionResult Return()
+        {
+            List<INV_StockHistory> model = new List<INV_StockHistory>();
+            model = db.INV_StockHistory.Where(e => e.Remarks == "Return").ToList();
+            return View(model);
+        }
+
+        public ActionResult AddReturnItem()
+        {
+            var model = new INV_InvoiceViewModal();
+            return View();
+        }
+
         public JsonResult RetrieveUserList()
         {
             try
@@ -107,8 +134,33 @@ namespace AnjaliMIS.Controllers
                         iNV_ItemConfiguration.UserID = Convert.ToInt16(Session["UserID"].ToString());
                     }
                     var inv_Item = db.INV_Item.Where(e => e.ItemID == iNV_ItemConfiguration.MainItemID).FirstOrDefault();
+
+                    string getIssueLastNumber;
                     if (inv_Item != null)
                     {
+                        
+                        var getIssueLast = db.INV_StockHistory.Where(e => e.Remarks == "Issue").OrderByDescending(e => e.StockHistoryID).FirstOrDefault();
+                        if (getIssueLast == null)
+                        {
+                            getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                        }
+                        else
+                        {
+                            var a= getIssueLast.IssueNumber;
+                            if (a == null) {
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01"; 
+                            }
+                            else {
+                                //a = "278201801";
+                                a = a.ToString();
+                                string delimiters = DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();
+                                //bool aaa = a.Contains(delimiters);
+                                string[] newstring= a.Split(new[] { delimiters }, StringSplitOptions.None);
+
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + (Convert.ToInt32(newstring[1])+1);
+                            }                            
+                        }
+                        
                         INV_StockHistory new_INV_StockHistory = new INV_StockHistory();
                         new_INV_StockHistory.ItemID = inv_Item.ItemID;
                         new_INV_StockHistory.OperationTypeID = 6;
@@ -117,8 +169,10 @@ namespace AnjaliMIS.Controllers
                         new_INV_StockHistory.UserID = iNV_ItemConfiguration.UserID;
                         new_INV_StockHistory.Created = DateTime.Now;
                         new_INV_StockHistory.Modified = DateTime.Now;
-                        new_INV_StockHistory.Remarks = null;
+                        new_INV_StockHistory.Remarks = "Issue";
                         new_INV_StockHistory.FinYearID = 2;
+                        
+                        new_INV_StockHistory.IssueNumber = getIssueLastNumber;
                         db.INV_StockHistory.Add(new_INV_StockHistory);
                         db.SaveChanges();
 
@@ -155,8 +209,33 @@ namespace AnjaliMIS.Controllers
 
                         var inv_Item = db.INV_Item.Where(e => e.ItemID == item.SubItemID).FirstOrDefault();
 
+                        string getIssueLastNumber;
                         if (inv_Item != null)
                         {
+                            var getIssueLast = db.INV_StockHistory.Where(e => e.Remarks == "Issue").OrderByDescending(e => e.StockHistoryID).FirstOrDefault();
+                            if (getIssueLast == null)
+                            {
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                            }
+                            else
+                            {
+                                var a = getIssueLast.IssueNumber;
+                                if (a == null)
+                                {
+                                    getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                                }
+                                else
+                                {
+                                    //a = "278201801";
+                                    a = a.ToString();
+                                    string delimiters = DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();
+                                    //bool aaa = a.Contains(delimiters);
+                                    string[] newstring = a.Split(new[] { delimiters }, StringSplitOptions.None);
+
+                                    getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + (Convert.ToInt32(newstring[1]) + 1);
+                                }
+                            }
+
                             INV_StockHistory new_INV_StockHistory = new INV_StockHistory();
                             new_INV_StockHistory.ItemID = inv_Item.ItemID;
                             new_INV_StockHistory.OperationTypeID = 3;
@@ -165,8 +244,10 @@ namespace AnjaliMIS.Controllers
                             new_INV_StockHistory.UserID = item.UserID;
                             new_INV_StockHistory.Created = DateTime.Now;
                             new_INV_StockHistory.Modified = DateTime.Now;
-                            new_INV_StockHistory.Remarks = null;
+                            new_INV_StockHistory.Remarks = "Issue";
                             new_INV_StockHistory.FinYearID = 2;
+
+                            new_INV_StockHistory.IssueNumber = getIssueLastNumber;
                             db.INV_StockHistory.Add(new_INV_StockHistory);
                             db.SaveChanges();
 
@@ -201,8 +282,34 @@ namespace AnjaliMIS.Controllers
                         iNV_ItemConfiguration.UserID = Convert.ToInt16(Session["UserID"].ToString());
                     }
                     var inv_Item = db.INV_Item.Where(e => e.ItemID == iNV_ItemConfiguration.MainItemID).FirstOrDefault();
+
+                    string getIssueLastNumber;
+
                     if (inv_Item != null)
                     {
+                        var getIssueLast = db.INV_StockHistory.Where(e => e.Remarks == "Return").OrderByDescending(e => e.StockHistoryID).FirstOrDefault();
+                        if (getIssueLast == null)
+                        {
+                            getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                        }
+                        else
+                        {
+                            var a = getIssueLast.IssueNumber;
+                            if (a == null)
+                            {
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                            }
+                            else
+                            {
+                                //a = "278201801";
+                                a = a.ToString();
+                                string delimiters = DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();
+                                //bool aaa = a.Contains(delimiters);
+                                string[] newstring = a.Split(new[] { delimiters }, StringSplitOptions.None);
+
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + (Convert.ToInt32(newstring[1]) + 1);
+                            }
+                        }
                         INV_StockHistory new_INV_StockHistory = new INV_StockHistory();
                         new_INV_StockHistory.ItemID = inv_Item.ItemID;
                         new_INV_StockHistory.OperationTypeID = 7;
@@ -211,8 +318,10 @@ namespace AnjaliMIS.Controllers
                         new_INV_StockHistory.UserID = iNV_ItemConfiguration.UserID;
                         new_INV_StockHistory.Created = DateTime.Now;
                         new_INV_StockHistory.Modified = DateTime.Now;
-                        new_INV_StockHistory.Remarks = null;
+                        new_INV_StockHistory.Remarks = "Return";
                         new_INV_StockHistory.FinYearID = 2;
+
+                        new_INV_StockHistory.IssueNumber = getIssueLastNumber;
                         db.INV_StockHistory.Add(new_INV_StockHistory);
                         db.SaveChanges();
 
@@ -249,8 +358,33 @@ namespace AnjaliMIS.Controllers
 
                         var inv_Item = db.INV_Item.Where(e => e.ItemID == item.SubItemID).FirstOrDefault();
 
+                        string getIssueLastNumber;
+
                         if (inv_Item != null)
                         {
+                            var getIssueLast = db.INV_StockHistory.Where(e => e.Remarks == "Return").OrderByDescending(e => e.StockHistoryID).FirstOrDefault();
+                            if (getIssueLast == null)
+                            {
+                                getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                            }
+                            else
+                            {
+                                var a = getIssueLast.IssueNumber;
+                                if (a == null)
+                                {
+                                    getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "01";
+                                }
+                                else
+                                {
+                                    //a = "278201801";
+                                    a = a.ToString();
+                                    string delimiters = DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString();
+                                    //bool aaa = a.Contains(delimiters);
+                                    string[] newstring = a.Split(new[] { delimiters }, StringSplitOptions.None);
+
+                                    getIssueLastNumber = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + (Convert.ToInt32(newstring[1]) + 1);
+                                }
+                            }
                             INV_StockHistory new_INV_StockHistory = new INV_StockHistory();
                             new_INV_StockHistory.ItemID = inv_Item.ItemID;
                             new_INV_StockHistory.OperationTypeID = 7;
@@ -259,8 +393,10 @@ namespace AnjaliMIS.Controllers
                             new_INV_StockHistory.UserID = item.UserID;
                             new_INV_StockHistory.Created = DateTime.Now;
                             new_INV_StockHistory.Modified = DateTime.Now;
-                            new_INV_StockHistory.Remarks = null;
+                            new_INV_StockHistory.Remarks = "Return";
                             new_INV_StockHistory.FinYearID = 2;
+
+                            new_INV_StockHistory.IssueNumber = getIssueLastNumber;
                             db.INV_StockHistory.Add(new_INV_StockHistory);
                             db.SaveChanges();
 
