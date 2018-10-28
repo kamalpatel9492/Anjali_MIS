58c0bdac2e52e0e44c5c898eced882c9fc442e2e
Vishal <vishalbhagiya@gmail.com>
2018-10-28 19:21:19 +0530

    adding reporting regarding reference and created item stock report

--- AnjaliMIS/CommonConfig.cs
+++ AnjaliMIS/CommonConfig.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AnjaliMIS.Models;
+using System.Reflection;
 
 namespace AnjaliMIS
 {
@@ -159,6 +160,32 @@ namespace AnjaliMIS
         public static int GetStatusComplete()
         {
             return 2;
+        }
+
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name);
+            }
+
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+
         }
     }
 }
---
+++ AnjaliMIS/Report/RPT_INV_Item_Stock.aspx.cs
@@ -0,0 +1,120 @@
+﻿using AnjaliMIS.Models;
+using AnjaliMIS.ViewModals;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AnjaliMIS.Report
+{
+    public partial class RPT_INV_Item_Stock : System.Web.UI.Page
+    {
+        DB_A157D8_AnjaliMISEntities1 db = new DB_A157D8_AnjaliMISEntities1();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //#region 11.1 Check User Login
+                //#endregion 11.1 Check User Login
+                FillDropDownList();
+            }
+        }
+
+        private void FillDropDownList()
+        {
+            List<INV_Category> Category = new List<INV_Category>();
+            Category = db.INV_Category.ToList();
+            DataTable dt = CommonConfig.ToDataTable(Category);
+            ddlCategoryID.DataSource = dt;
+            ddlCategoryID.DataValueField = "CategoryID";
+            ddlCategoryID.DataTextField = "CategoryName";
+            ddlCategoryID.DataBind();
+            ddlCategoryID.Items.Insert(0, new ListItem(" Select Category", "-99"));
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            SqlInt32 CategoryID = SqlInt32.Null;
+            String CategoryName = String.Empty;
+            DataTable dtItem = new DataTable();
+            List<ItemStockReportViewModal> _ItemStockReportViewModal = new List<ItemStockReportViewModal>();
+
+            if (ddlCategoryID.SelectedIndex>0)
+            {
+                CategoryID = Convert.ToInt32(ddlCategoryID.SelectedValue);
+                CategoryName = ddlCategoryID.SelectedItem.Text;
+            }
+
+            using (DB_A157D8_AnjaliMISEntities1 db = new DB_A157D8_AnjaliMISEntities1())
+            {
+                try
+                {
+                    var sql = "exec PP_INV_Item_SelectItemStockReportByCategoryID @CategoryID,@CompanyID";
+
+                    List<SqlParameter> parameterList = new List<SqlParameter>();
+                    parameterList.Add(new SqlParameter("@CategoryID", CategoryID));
+                    parameterList.Add(new SqlParameter("@CompanyID", CommonConfig.GetCompanyID()));
+
+                    SqlParameter[] parameters = parameterList.ToArray();
+
+                    dtItem = CommonConfig.ToDataTable(db.Database.SqlQuery<ItemStockReportViewModal>(sql, parameters).ToList());
+
+                    //_ItemStockReportViewModal = data1.Select(dataElement => new ItemStockReportViewModal()
+                    //{
+                    //    ModuleID = dataElement.ModuleID,
+                    //    ModuleName = dataElement.ModuleName,
+                    //    Controller = dataElement.Controller,
+                    //    Action = dataElement.Action,
+                    //    URL = dataElement.URL,
+                    //    IconName = dataElement.IconName,
+                    //    StrictlyStop = dataElement.StrictlyStop,
+                    //    Sequence = dataElement.Sequence,
+                    //    UserID = dataElement.UserID
+                    //}).ToList();
+                }
+                catch (Exception ex)
+                {
+                    //SaveErrorLog("JobRepository", "GetJobListByPaging", ex);
+
+                }
+            }
+
+            //using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_A157D8_AnjaliMISEntities1"].ConnectionString))
+            //{
+            //    using (SqlCommand cmd = new SqlCommand("PP_INV_Item_SelectItemStockReportByCategoryID", con))
+            //    {
+            //        cmd.CommandType = CommandType.StoredProcedure;
+
+            //        cmd.Parameters.Add("@CategoryID", SqlDbType.Int).Value = CategoryID;
+            //        cmd.Parameters.Add("@CompanyID", SqlDbType.Int).Value = CommonConfig.GetCompanyID();
+
+            //        con.Open();
+            //        SqlDataReader dr = cmd.ExecuteReader();
+            //        dtItem.Load(dr);
+            //    }
+            //}
+
+
+            rvReport.Reset();
+
+            rvReport.LocalReport.DataSources.Clear();
+            rvReport.ProcessingMode = ProcessingMode.Local;
+            rvReport.LocalReport.EnableExternalImages = true;
+            rvReport.LocalReport.ReportPath = Server.MapPath("~/Report/RPT_INV_Item_Stock.rdlc");
+            rvReport.LocalReport.DataSources.Add(new ReportDataSource("PP_INV_Item_SelectItemStockReportByCategoryID", dtItem));
+
+            rvReport.Visible = true;
+            rvReport.LocalReport.DisplayName = "Item Stock Report";
+            rvReport.LocalReport.Refresh();
+            rvReport.DataBind();
+        }
+    }
+}
